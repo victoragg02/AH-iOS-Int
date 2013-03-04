b442a55c52d6f997c6c4ce5415a5876399c2d292
Wojciech Kedzierski <wkedzierski@utest.com>
2013-03-04 14:11:08 +0100

    Updated to Apphance 1804

--- Apphance-Pre-Production.framework/Versions/A/Headers/APHLogger.h
+++ Apphance-Pre-Production.framework/Versions/A/Headers/APHLogger.h
@@ -1,9 +1,8 @@
 //
-//  Apphance-iOS.h
-//  Apphance-iOS
+//  Apphance-Pre-Production
 //
-//  Created by Piotr Wach, Paul Dudek on 10-01-25.
-//  Copyright 2010 Apphance. All rights reserved.
+//  Created by Piotr Wach, Paul Dudek, Pawel Janeczek on 10-01-25.
+//  Copyright 2013 uTest. All rights reserved.
 //
 //	This is the main logger file. Include it in your project
 //  along with the Apphance Framework. 
@@ -12,21 +11,23 @@
 #import <UIKit/UIKit.h>
 #import "APHDefines.h"
 
-/* 
- * Enum preseting possible logging level
- */
+/**
+* Enum presenting possible logging level
+*/
 typedef enum {
-	APHLogLevelFatal = 16,
-	APHLogLevelError = 8,
-	APHLogLevelWarning = 4,
-	APHLogLevelInfo = 2,
-	APHLogLevelVerbose = 0
+    APHLogLevelFatal = 16,
+    APHLogLevelError = 8,
+    APHLogLevelWarning = 4,
+    APHLogLevelInfo = 2,
+    APHLogLevelVerbose = 0
 } APHLogLevel;
 
-/*
- * Convinience method to log applications exceptions
- */
-APH_EXTERN void APHUncaughtExceptionHandler(NSException* exception);
+/**
+* Convinience method to log applications exceptions
+*/
+APH_EXTERN
+
+void APHUncaughtExceptionHandler(NSException *exception);
 
 /** Logging macros **/
 
@@ -34,95 +35,133 @@ APH_EXTERN void APHUncaughtExceptionHandler(NSException* exception);
  * Replace all occurences of NSLog with APHLog. 
  * Except for working like normal log it will also send message to Apphance server.
  */
-#define APHLog(nsstring_format, ...)	\
-	do {						\
-		[APHLogger logWithLevel:APHLogLevelInfo \
-		tag:nil \
-		line:__LINE__ fileName:[NSString stringWithUTF8String:__FILE__] \
-		method:[NSString stringWithUTF8String:__PRETTY_FUNCTION__] \
-		stacktrace:[NSThread callStackReturnAddresses]\
-		format:nsstring_format, \
-		##__VA_ARGS__];\
+#define APHLog(nsstring_format, ...)    \
+    do {                        \
+        [APHLogger logWithLevel:APHLogLevelInfo \
+        tag:nil \
+        line:__LINE__ fileName:[NSString stringWithUTF8String:__FILE__] \
+        method:[NSString stringWithUTF8String:__PRETTY_FUNCTION__] \
+        stacktrace:[NSThread callStackReturnAddresses]\
+        format:nsstring_format, \
+        ##__VA_ARGS__];\
 } while(0)
 
 /*
  * Works as the one above, except it provides additional configuration options
  */
-#define APHExtendedLog(APHLogLevel, nsstring_tag, nsstring_format, ...)	\
-	do {						\
-		[APHLogger logWithLevel:(APHLogLevel) \
-		tag:(nsstring_tag) \
-		line:__LINE__ fileName:[NSString stringWithUTF8String:__FILE__] \
-		method:[NSString stringWithUTF8String:__PRETTY_FUNCTION__] \
-		stacktrace:[NSThread callStackReturnAddresses] \
-		format:nsstring_format, \
-		##__VA_ARGS__];\
+#define APHExtendedLog(APHLogLevel, nsstring_tag, nsstring_format, ...)    \
+    do {                        \
+        [APHLogger logWithLevel:(APHLogLevel) \
+        tag:(nsstring_tag) \
+        line:__LINE__ fileName:[NSString stringWithUTF8String:__FILE__] \
+        method:[NSString stringWithUTF8String:__PRETTY_FUNCTION__] \
+        stacktrace:[NSThread callStackReturnAddresses] \
+        format:nsstring_format, \
+        ##__VA_ARGS__];\
 } while(0)
 
+
+//! Main apphance API class
+/**
+* Main apphance API class
+* @see https://help.apphance.com/library-installation/ios/tutorial
+ */
 @interface APHLogger : NSObject {
-	
+
 }
 
 /** General setup methods **/
 
-/*
- * This will override current application version name. 
- * Default one is obtained by CFBundleShortVersionString. 
- * Be sure to call this before starting Apphance session otherwise it will have no effect. 
+/**
+* This will override current application version name.
+* Default one is obtained by CFBundleShortVersionString.
+* Be sure to call this before starting Apphance session otherwise it will have no effect.
+* @param versionName Application version name
  */
-+ (void) setVersionName:(NSString*) versioName;
++ (void)setVersionName:(NSString *)versionName;
 
-/*
- * This will override current application version code. 
- * Default one is obtained by CFBundleVersion. 
- * Be sure to call this before starting Apphance session otherwise it will have no effect. 
+/**
+* This will override current application version code.
+* Default one is obtained by CFBundleVersion.
+* Be sure to call this before starting Apphance session otherwise it will have no effect.
+* @param versionNumber Application version number
  */
-+ (void) setVersionNumber:(NSString*) setVersionNumber;
++ (void)setVersionNumber:(NSString *)versionNumber;
 
-/*
- * Logs exception. Screenshot will be included in the data sent to 
- * the server. You can use APHUncaughtExceptionHandler as a convinience accessor to this method.
+/**
+* Logs exception. Screenshot will be included in the data sent to the server.
+* You can use APHUncaughtExceptionHandler as a convenience accessor to this method.
+* @param error Exception that will be passed to Apphance
  */
-+ (void) logApplicationException:(NSException *) error;
++ (void)logApplicationException:(NSException *)error;
 
-/*
- * This method will register object for logging, meaning each and
- * every method sent to it will be logged, including timestamp.
- * See documentation for more information.
+/**
+* This method will register object for logging, meaning each and every method sent to it will be logged, including timestamp.
+* See documentation for more information.
+* @param object Object that will be registered for logging
  */
-+ (id) registerObjectForLogging:(id) object;
++ (id)registerObjectForLogging:(id)object;
 
-/*
- * Forces the contents of the session log buffer to be send. 
+/**
+* Forces the contents of the session log buffer to be send.
  */
-+ (void) flush;
++ (void)flush;
 
 /** Starting new session **/
 
-/*
- * Starts APH session. Should be called once per application run - doing otherwise will result in an undefined behavior.
- * You should pass a valid apphance mode.
+/**
+* Starting APH session. Should be called once per application run - doing otherwise will result in an undefined behavior.
+* @param applicationID Application ID that you can get from Apphance
+* @param apphanceMode Apphance mode:kAPHApphanceModeQA, kAPHApphanceModeSilent
+* @param withUtest uTest enabled
  */
-+ (void) startNewSessionWithApplicationKey:(NSString *)applicationID apphanceMode:(NSString*) apphanceMode;
++ (void)startNewSessionWithApplicationKey:(NSString *)applicationID apphanceMode:(NSString *)apphanceMode withUtest:(BOOL)withUtest;
 
-+ (void) startNewSessionWithApplicationKey:(NSString *)applicationID apphanceMode:(NSString*) apphanceMode withUtest:(BOOL) withUtest;
-
-/** Market mode specific **/
-
-/*
- * Calling this method will force apphance to upload session data even if there was no crash in it.
- * Works only in Market mode. 
+/**
+* uTest is disabled by default.
+* @see APHLogger#startNewSessionWithApplicationKey:apphanceMode:withUtest:
  */
-- (void) forceSessionUploadOnNextStartup;
++ (void)startNewSessionWithApplicationKey:(NSString *)applicationID apphanceMode:(NSString *)apphanceMode;
+
+/**
+* Default mode kAPHApphanceModeQA.
+* uTest is disabled by default.
+* @see APHLogger#startNewSessionWithApplicationKey:apphanceMode:withUtest:
+ */
++ (void)startNewSessionWithApplicationKey:(NSString *)applicationID;
+
+/**
+* Only available on Production library.
+* @see https://help.apphance.com/library-installation/ios/tutorial
+*/
++ (void)feedback:(NSString *)title placeholder:(NSString *)placeholder;
+
+/**
+* Only available on Production library.
+* @see https://help.apphance.com/library-installation/ios/tutorial
+*/
++ (void)feedback:(NSString *)title;
+
+/**
+* Only available on Production library.
+* @see https://help.apphance.com/library-installation/ios/tutorial
+*/
++ (void)feedback;
+
+/**
+* Only available on Production library.
+* @see https://help.apphance.com/library-installation/ios/tutorial
+*/
++ (void)sendFeedback:(NSString *)feedback;
 
 @end
 
-/*
- * Despite being called private, you can generaly call these method, if you wish. However, this is strongly discouraged, since given macros and functions
- * are more conviniet way of using APH. 
+/**
+* Despite being called private, you can generally call these method, if you wish.
+* However, this is strongly discouraged, since given macros and functions are more convenient way of using APH.
  */
-@interface APHLogger (PrivateAccessors) 
+@interface APHLogger (PrivateAccessors)
 
-+ (void) logWithLevel:(APHLogLevel) level tag:(NSString *) tag line:(NSInteger) line fileName:(NSString *)fileName method:(NSString *) method stacktrace:(NSArray*)stacktrace format:(NSString *)format, ...;
++ (void)logWithLevel:(APHLogLevel)level tag:(NSString *)tag line:(NSInteger)line fileName:(NSString *)fileName method:(NSString *)method stacktrace:(NSArray *)stacktrace format:(NSString *)format, ...;
 
 @end
